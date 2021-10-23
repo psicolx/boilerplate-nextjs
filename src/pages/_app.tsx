1937bcd2d4796f0022d8e1d0027a701837dcb5d2
WanderleyJunior <wanderleyjunr@gmail.com>
2021-10-23 00:42:15 -0300

    initializantion of app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/global'
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <title>Psicolx - Plataforma da Saúde Emocional</title>
+        {/* <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" /> */}
+        {/* <link rel="manifest" href="/manifest.json" /> */}
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Psicolx onde Psicologos curam a Saúde Emocional"
         />
       </Head>
-      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
